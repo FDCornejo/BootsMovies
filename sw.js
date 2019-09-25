ab79081e1791226a972d0af4b67f1c6b09370ea6
Daniel <fdanielcm@gmail.com>
2019-09-25 17:39:00 -0500

    More Icons

---
+++ sw.js
@@ -0,0 +1,30 @@
+  
+var cacheName = 'filme-1.0.0';
+var dataCacheName = 'filme-data-v1';
+
+var urlsToCache = [];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
